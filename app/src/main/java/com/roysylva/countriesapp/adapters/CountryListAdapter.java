d26bb4db25859bb35a23a2a41d3556a480324822
Roy Sylvanus <vicevanus@gmail.com>
2022-02-14 15:21:49 +0100

    Adapter implementation

---
+++ app/src/main/java/com/roysylva/countriesapp/adapters/CountryListAdapter.java
@@ -0,0 +1,66 @@
+package com.roysylva.countriesapp.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.roysylva.countriesapp.R;
+import com.roysylva.countriesapp.databinding.ItemCountryBinding;
+import com.roysylva.countriesapp.models.CountryModel;
+
+import java.util.List;
+
+public class CountryListAdapter extends RecyclerView.Adapter<CountryListAdapter.CountryViewHolder> {
+
+    private List<CountryModel> countriesList;
+
+    public CountryListAdapter(List<CountryModel> countriesList) {
+        this.countriesList = countriesList;
+    }
+
+    public void updateCountries(List<CountryModel> newCountriesList){
+        countriesList.clear();
+        countriesList.addAll(newCountriesList);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CountryViewHolder holder, int position) {
+       CountryModel countryModel = countriesList.get(position);
+
+       holder.item.name.setText(countryModel.getCountryName());
+       holder.item.capital.setText(countryModel.getCapital());
+
+
+    }
+
+    @NonNull
+    @Override
+    public CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        ItemCountryBinding binding = ItemCountryBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+
+        return new  CountryViewHolder(binding);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return countriesList.size();
+    }
+
+    class CountryViewHolder extends RecyclerView.ViewHolder{
+
+        private ItemCountryBinding item;
+
+        public CountryViewHolder(@NonNull ItemCountryBinding binding) {
+            super(binding.getRoot());
+            this.item = binding;
+
+        }
+    }
+}
--- app/src/main/java/com/roysylva/countriesapp/ui/MainActivity.java
+++ app/src/main/java/com/roysylva/countriesapp/ui/MainActivity.java
@@ -1,16 +1,44 @@
 package com.roysylva.countriesapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.roysylva.countriesapp.R;
+import com.roysylva.countriesapp.adapters.CountryListAdapter;
+import com.roysylva.countriesapp.databinding.ActivityMainBinding;
+import com.roysylva.countriesapp.viewmodels.CountryViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+     ActivityMainBinding binding;
+    TextView textView;
+    ProgressBar progressBar;
+    RecyclerView recyclerView;
+    private CountryViewModel countryViewModel;
+    private CountryListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        progressBar = (ProgressBar) findViewById(R.id.loading_View);
+        textView = (TextView) findViewById(R.id.list_error);
+
+        countryViewModel = ViewModelProviders.of(this).get(CountryViewModel.class);
+
+
+
+
+
+
     }
 }
